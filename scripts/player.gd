ea4e16964dd460026744a15ecbffe6cadb84bcd3
notQuiteTrash <notquitetrash@pm.me>
2026-02-02 16:09:37 -0700

    finished run animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody3D 
+@onready var anim_player: AnimationPlayer = $Mesh/AnimationPlayer
 
 @export var speed : float = 5.0
 # You type the above line like this as well: @export var speed:= 5.0
@@ -13,6 +14,7 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
 
 	# Handle jump.
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():  
@@ -33,6 +35,18 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	turn_to(direction)
 
+	var current_speed := velocity.length()
+	const RUN_SPEED := 4.0
+	const BLEND_SPEED := 0.2
+	
+	if current_speed > RUN_SPEED :
+		anim_player.play("freehand_run", BLEND_SPEED) 
+	elif current_speed > 0.0 :
+		anim_player.play("freehand_walk", BLEND_SPEED, lerp(0.5, 1.75, current_speed / RUN_SPEED))
+	else: 
+		anim_player.play("freehand_idle")
+	
+		
 func turn_to(direction: Vector3) -> void:
 	if direction.length() > 0 :
 		var yaw := atan2(-direction.x,-direction.z)
